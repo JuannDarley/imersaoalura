e9e0d8edc327dbde1f7af7d9a96f013f1650dc3d
JuannDarley <juandarley99@gmail.com>
2024-11-18 20:09:53 -0300

    Create new server with imersao alura

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+
+const app = express()
+app.listen(3000, () =>{
+
+  console.log('Servidor escutando!')
+  
+})
+
+app.get('/api', (req, res) => {
+
+  res.status(200).send('Eu te amo minha vida <3')
+
+})
+
+app.get('/livro', (req, res) => {
+
+  res.status(200).send({
+    "titulo": "O Senhor dos Anéis",
+    "autor": "J.R.R. Tolkien",
+    "ano": 1954,
+    "genero": "Fantasia"
+  })
+})
