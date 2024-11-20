94bc2dc482a604eea597645d99f8302c91c2bfb4
JuannDarley <juandarley99@gmail.com>
2024-11-20 15:22:21 -0300

    Created conection database Mongo

--- server.js
+++ server.js
@@ -1,54 +1,70 @@
 import express from 'express'
+import connectToBank from './src/config/dbConfig.js'
+
+// Conection with Database Mongo
+const conection = await connectToBank(process.env.STRING_CONECTION)
 
 // Variable data base 
 const posts = [
 
     { id: 1,
-      descricao: 'Uma foto teste', 
-      imagem: 'https://placecats.com/millie/300/150' },
+      description: 'A test photo', 
+      image: 'https://placecats.com/millie/300/150' },
 
     { id: 2,
-      descricao: 'Paisagem incrível', 
-      imagem: 'https://source.unsplash.com/random' },
+      description: 'Incredible landscape', 
+      image: 'https://source.unsplash.com/random' },
 
     { id: 3,
-      descricao: 'Gato brincando com um novelo de lã',
-       imagem: 'https://placekitten.com/400/300' },
+      description:'Cat playing with a ball of yarn',
+      image: 'https://placekitten.com/400/300' },
 
     { id: 4,
-      descricao: 'Cachorro fazendo uma pose fofa',
-      imagem: 'https://placepuppy.com/200/300' },
+      description: 'Dog doing a cute pose' ,
+      image: 'https://placepuppy.com/200/300' },
 
     { id: 5,
-      descricao: 'Comida deliciosa',
-       imagem: 'https://unsplash.com/photos/food' },
+      description: 'Delicious food',
+      image: 'https://unsplash.com/photos/food' },
 
     { id: 6,
-      descricao: 'Cidade à noite',
-      imagem: 'https://source.unsplash.com/city' },
+      description: 'City at night',
+      image: 'https://source.unsplash.com/city' },
 
     { id: 7,
-      descricao: "Um pôr do sol espetacular", 
-      imagem: 'https://unsplash.com/nature/sunset' }
+      description: 'A spectacular sunset', 
+      image: 'https://unsplash.com/nature/sunset' }
 ]
 
-
+// Create conection Server
 const app = express()
 
 app.use(express.json())
 
 app.listen(3000, () =>{
 
-  console.log('Servidor escutando!')
+  console.log('Server listening')
   
 })
 
-app.get('/posts', (req, res) =>
-  {
+// Function connection with table
+async function getAllPosts(){
+  
+  const db = conection.db('imersao-instabytes')
+  const colection = db.collection('posts')
+  return colection.find().toArray()
+
+}
+
+//Route for server
+app.get('/posts', async (req, res) => {
+
+  const posts = await getAllPosts()
   res.status(200).json(posts)
+
 })
 
-function buscarPostPorId(id){
+function searchPostById(id){
 
   return posts.findIndex((post) => {
 
@@ -58,7 +74,7 @@ function buscarPostPorId(id){
 
 app.get('/posts/:id', (req, res) => {
 
-  const index = buscarPostPorId(req.params.id)
+  const index = searchPostById(req.params.id)
 
   res.status(200).json(posts[index])
 
